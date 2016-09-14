57f6a422a0a1836df13f99d49a7cf2a9132df677
mattyx14 <mattyx14@users.noreply.github.com>
2016-09-13 19:46:40 -0500

    misstake here

--- data/movements/scripts/citizen.lua
+++ data/movements/scripts/citizen.lua
@@ -5,6 +5,13 @@ function onStepIn(creature, item, position, fromPosition)
 			return false
 		end
 
+		if town:getId() == 12 and player:getStorageValue(Storage.BarbarianTest.Questline) < 8 then
+			player:sendTextMessage(MESSAGE_STATUS_WARNING, 'You first need to absolve the Barbarian Test Quest to become citizen!')
+			player:teleportTo(town:getTemplePosition())
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			return true
+		end
+
 		local town = Town(item.actionid - 30020)
 		player:setTown(town)
 		player:sendTextMessage(MESSAGE_INFO_DESCR, "You are the newest resident of " .. town:getName(town) .. ".")
