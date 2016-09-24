674fcc7471a2425b285855bd800347e7990d607d
murilao <colascoreglobal@gmail.com>
2016-09-23 21:54:23 -0300

    Fixed freeblesslowlevel

--- data/creaturescripts/scripts/others/freeblesslowlevel.lua
+++ data/creaturescripts/scripts/others/freeblesslowlevel.lua
@@ -1,13 +1,13 @@
 local freeBlessMaxLevel = 130
 
-function onLogin(player)
+function onLogin(cid)
+    local player = Player(cid)
 	if player:getLevel() <= freeBlessMaxLevel then
 		for i = 1, 6 do
 			player:addBlessing(i)
 		end
+	    player:say('FREE BLESS LVL ' .. freeBlessMaxLevel .. '.', TALKTYPE_ORANGE_1)
+	    player:getPosition():sendMagicEffect(CONST_ME_HOLYDAMAGE)
 	end
-
-	player:getPosition():sendMagicEffect(CONST_ME_HOLYDAMAGE)
-	player:say('FREE BLESS LVL ' .. freeBlessMaxLevel .. '.', TALKTYPE_ORANGE_1)
 	return true
 end
