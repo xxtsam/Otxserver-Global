bf389bd92e92119b76238a1638e6fd4d86ab7611
marcelo <marcelo_araujo999@hotmail.com>
2016-09-24 15:52:43 -0300

    Update ferumbras7.lua
    FIX PARA FERUMBRAS QUEST FUNCIONAR

--- data/movements/scripts/others/ferumbras7.lua
+++ data/movements/scripts/others/ferumbras7.lua
@@ -4,8 +4,10 @@ function onStepIn(creature, item, position, fromPosition)
 		return true
 	end
 	if player:getStorageValue(51997) >= 1 then
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "This room has been completed, go to next.")
 	else
-		player:teleportTo(fromPosition)
+		player:teleportTo(Position(33309, 32377, 13))
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You escaped. To complete this part of the mission It is necessary to kill the boss.")
 	end
 	return true
 end
